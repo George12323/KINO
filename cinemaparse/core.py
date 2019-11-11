9aa95c7e28f9bc8f1ae67a2d6f38374e170e766b
George12323 <53345504+George12323@users.noreply.github.com>
2019-11-11 11:55:44 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -32,6 +32,27 @@ class Kinoparser:
             list_of_all_films.append(film["attr-title"])
 
         return list_of_all_films[1:-1]
+     def get_film_nearest_session(self, film):
+        page = requests.get('https://{}.subscity.ru/'.format(self.town))
+        soup = BeautifulSoup(page.text, 'html.parser')
+        names = soup.find_all(class_="movie-plate")
+        for name in names:
+            if name.get('attr-title') == film:
+                url = 'https://{}.subscity.ru/'.format(self.town) + name.find('a').get('href')
+                page = requests.get(url)
+                soup = BeautifulSoup(page.text, 'html.parser')
+                cinemas = soup.find_all(class_="row-entity")
+                time_cinema = dict()
+                for cinema in cinemas:
+                    for t in cinema.find_all(class_='text-center cell-screenings'):
+                        t = int(t.get('attr-time'))
+                        name = cinema.find(class_='underdashed').text
+                        if time.time() < t:
+                            time_cinema[t + 3600 * 3] = name
+                time_cinema = sorted(time_cinema.items(), key=lambda key: key[0])
+                if len(time_cinema) == 0 or datetime.utcfromtimestamp(time_cinema[0][0]).strftime('%x') != datetime.today().strftime('%x'):
+                    return None, None
+                return time_cinema[0][1], datetime.utcfromtimestamp(time_cinema[0][0]).strftime('%H:%M')
 
 KINO_IN_MOSCOW = Kinoparser('msk')
 KINO_IN_MOSCOW.extract_raw_content()
