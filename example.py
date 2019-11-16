81c76aceb33ab39ecc6a0c11ff555a8b268102f0
George12323 <53345504+George12323@users.noreply.github.com>
2019-11-16 21:09:28 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
-from cinemaparse import core
+from cinemaparse import Kinoparser
+
 KP = core.Kinoparser(self, city)
-KP.exstract_raw_content()
+MSK_PARSER = KP('msk')
+MSK_PARSER.extract_raw_content()
+MSK_PARSER.print_raw_content()
